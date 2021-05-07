6bcc361224ea7492dd0ba2ca9da476178665e280
bala kaushik ganguneni <51963897+bkganguneni@users.noreply.github.com>
2021-05-07 15:04:32 +0530

    Add files via upload

---
+++ CSES/fibonacci series.py
@@ -0,0 +1,6 @@
+n = int(input())
+def fib(n, computed = {0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    return computed[n]%((10**9)+7)
+print(fib(n))
