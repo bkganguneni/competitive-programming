4b136a058d390dfddd328a9111ce1bd008e7b226
bala kaushik ganguneni <51963897+bkganguneni@users.noreply.github.com>
2021-05-09 19:06:12 +0530

    Update XOR EQUALITY.py

--- MAY CODECHEF LONG CHALLANGE/XOR EQUALITY.py
+++ MAY CODECHEF LONG CHALLANGE/XOR EQUALITY.py
@@ -2,7 +2,7 @@ def power(x, y):
     m = 10**9 + 7
     res = 1   
     while (y > 0):
-        if ((y & 1) != 0):
+        if (y & 1):
             res = res * x % m
         y = y >> 1 
         x = x * x % m  
